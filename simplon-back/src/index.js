8eca2b044eacaaa184266a5e85fc428a7ae43300
Nabil <nabil.mirabeau@hotmail.fr>
2024-02-23 11:15:57 +0100

    ajout de la route HomePage

--- simplon-back/src/index.js
+++ simplon-back/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+dotenv.config();
+const HomeRouter = require("./routes/HomeRoute");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/", HomeRouter);
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
